cfff081d33894359ebb6cdc5e226c3f0f3b27da8
hunisG <soretiwb@gmail.com>
2025-12-23 12:42:42 -0600

    package.json and vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  base: "/AmazonClone/amazon_replica/Amazon-Frontend-Replica-25-26",
+  plugins: [react()]
+
 })

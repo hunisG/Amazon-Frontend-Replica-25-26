a3573aac2bf8b706a619e5e8bfd255a6e9bf3d49
hunisG <soretiwb@gmail.com>
2025-12-23 11:55:52 -0600

    Set homepage and base for GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/{Amazon-Frontend-Replica-25-26}",
   plugins: [react()],
-})
+});

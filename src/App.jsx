34ac45678f26d0212ccc870d3c94a345c1413e26
hunisG <soretiwb@gmail.com>
2025-12-23 11:32:01 -0600

    Frontend completed

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Routing from './Routing'
+import './index.css'
+import {BrowserRouter as Router } from 'react-router-dom'
+
+
+function App() {
+  return (
+    <Router>
+
+<Routing/>
+
+
+    </Router>
+  )
+}
+
+export default App

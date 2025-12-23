962b47ec626050234196c14319774347be175155
hunisG <soretiwb@gmail.com>
2025-12-23 15:40:14 -0600

    Fix Vite base and router basename

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,17 @@
 import React from 'react'
 import Routing from './Routing'
 import './index.css'
-import {BrowserRouter as Router } from 'react-router-dom'
+
 
 
 function App() {
   return (
-    <Router>
+    
 
 <Routing/>
 
 
-    </Router>
+    
   )
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -4,15 +4,17 @@ import App from './App.jsx'
 import React from 'react';
 import { DataProvider } from './Components/DataProvider/DataProvider.jsx';
 import { Reducer, initialState } from './Utility/Reducer.jsx';
+import { BrowserRouter } from 'react-router-dom';
 
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
 
     {/* <DataProvider Reducer ={Reducer} initialState={initialState}> */}
+    <BrowserRouter basename='/Amazon-Frontend-Replica-25-26'>
 <DataProvider>
        <App />
    
     </DataProvider>
-
+</BrowserRouter>
   </React.StrictMode>
 ); 
